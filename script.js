df20d2b79cdefb7cb1b7dcc05025c98dc6f9acb0
DataManya <38845075+DataManya@users.noreply.github.com>
2022-01-14 12:18:24 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-function ip2long( ip_str )
-{
-    var parts = ip_str.split( "." );    
-    return parts.reduce( function( x, y ) 
-    {
-        return ( +y ) + x * 256; 
-    } );
+function scope(x, up=window) {
+    return new Proxy(x, {
+        has: (o, k) => true,
+        get: (o, k) => k in o ? o[k] : up[k]
+    });
 }
 
-function ip_range( ip1, ip2 )
-{
-    var ip1 = ip2long( ip1 );
-    var ip2 = ip2long( ip2 );
-result = 1 + Math.abs( ip2 - ip1 );
-    alert("В диапазоне от первого ip-адреса до второго ip-адреса " + result + " адресов");
+var Example = {};
+
+with (scope(Example)) {
+    x = 1;
+    y = 2;
+    add_x = function(y) {
+        console.log(x + y);
+    }
 }
 
+Example.add_x(5);
+Example.add_x(20);
 
+
+with (scope(Example)) {
+    x = 3;
+    add_y = function(x) {
+        console.log(x + y);
+    }
+}
+
+Example.add_x(5);
+
+Example.y = 5;
+Example.add_y(30);
+
+Example.y = 1;
+Example.add_y(30);
